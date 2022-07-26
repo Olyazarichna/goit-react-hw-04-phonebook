49524688cd4bc06ad5509cffc4de104458c6e401
Olyazarichna <olya.zarichna@gmail.com>
2022-07-26 22:24:42 +0300

    hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,134 @@
-export const App = () => {
+import React from 'react';
+import { useState, useEffect } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+
+export function App() {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const haveContacts = (contacts, data) => {
+    return contacts.some(
+      contact => contact.name.toLowerCase() === data.name.toLowerCase()
+    );
+  };
+
+  const formSubmitHandler = data => {
+    if (!haveContacts(contacts, data)) {
+      setContacts([data, ...contacts]);
+    } else {
+      alert(`${data.name} is already exist`);
+    }
+  };
+
+  const findContacts = () => {
+    const searching = filter.toLowerCase();
+    const findContact = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(searching)
+    );
+    return findContact;
+  };
+
+  const filterChange = event => {
+    setFilter(event.target.value);
+  };
+
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} />
+
+      <h2>Contacts</h2>
+
+      <Filter onChange={filterChange} value={filter} />
+      <ContactList contacts={findContacts()} onDeleteContact={deleteContact} />
     </div>
   );
-};
+}
+
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+//   componentDidMount() {
+//     let contacts = localStorage.getItem('contacts');
+//     let parseContacts = JSON.parse(contacts);
+//     if (contacts) {
+//       this.setState({ contacts: parseContacts });
+//     }
+//   }
+
+//   componentDidUpdate(prevProps, prevState) {
+//     if (this.state.contacts !== prevState.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   haveContacts = (contacts, data) => {
+//     return contacts.some(
+//       contact => contact.name.toLowerCase() === data.name.toLowerCase()
+//     );
+//   };
+
+//   formSubmitHandler = data => {
+//     this.setState(({ contacts }) => {
+//       if (!this.haveContacts(contacts, data)) {
+//         return { contacts: [data, ...contacts] };
+//       } else {
+//         alert(`${data.name} is already exist`);
+//       }
+//     });
+//   };
+
+//   findContacts() {
+//     const searching = this.state.filter.toLowerCase();
+//     const findContact = this.state.contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(searching)
+//     );
+//     return findContact;
+//   }
+
+//   filterChange = event => {
+//     const { name, value } = event.currentTarget;
+//     this.setState({ [name]: value });
+//   };
+//   deleteContact = id => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(contact => contact.id !== id),
+//     }));
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={this.formSubmitHandler} />
+
+//         <h2>Contacts</h2>
+
+//         <Filter onChange={this.filterChange} value={this.state.filter} />
+//         <ContactList
+//           contacts={this.findContacts()}
+//           onDeleteContact={this.deleteContact}
+//         />
+//       </div>
+//     );
+//   }
+// }
